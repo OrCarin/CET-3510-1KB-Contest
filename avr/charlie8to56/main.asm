756695796c5922763b998a73a3ef89acdf63ccc0
Darius-F <darius.freeman6@icloud.com>
2016-12-11 21:10:52 -0500

    Darius 7 segment Charlieplex

--- avr/charlie8to56/main.asm
+++ avr/charlie8to56/main.asm
@@ -29,7 +29,7 @@
 	.nolist
 	.include "./m328Pdef.inc" ;Include definitions file
 	.list
-
+ 
 	.def temp = r16 	;Use r16 as temporary register
 	.def countOF = r17 	;Use r17 as overflow counter
 	.def leds1 = r18 	;Use r18 as output LEDS
@@ -42,11 +42,6 @@
 	.org 0x0000		;Set jump for reset
 	rjmp RESET
 
-	.org 0x0003
-	rjmp BUTTON1
-
-	.org 0x0004
-	rjmp BUTTON2
 	
 	.org 0x0020		;Set jump for overflow interupt
 	rjmp OVERFLOW
@@ -123,3 +118,93 @@ DELAY:				;Wait about r16/60 seconds
 OVERFLOW:
 	inc countOF
 	reti			;Return after interupt
+
+DISPLAY1_N:	
+	ldi disp1, 0b00000100
+	ret
+
+DISPLAY1_0:
+	ldi disp1, 0b11111110
+	ret
+
+DISPLAY1_1:
+	ldi disp1, 0b01100100
+	ret
+
+DISPLAY1_2:
+	ldi disp1, 0b11011101
+	ret
+	
+DISPLAY1_3:
+	ldi disp1, 0b11110101
+	ret
+
+DISPLAY1_4:
+	ldi disp1, 0b01100111
+	ret
+
+DISPLAY1_5:
+	ldi disp1, 0b10110111
+	ret
+
+DISPLAY1_6:
+	ldi disp1, 0b10111111
+	ret
+
+DISPLAY1_7:
+	ldi disp1, 0b11100100
+	ret
+
+DISPLAY1_8:
+	ldi disp1, 0b11111111
+	ret
+
+DISPLAY1_9:
+	ldi disp1, 0b11100111
+	ret
+
+
+	
+DISPLAY2_N:	
+	ldi disp1, 0b00001000
+	ret
+
+DISPLAY2_0:
+	ldi disp1, 0b11111110
+	ret
+
+DISPLAY2_1:
+	ldi disp1, 0b01101000
+	ret
+
+DISPLAY2_2:
+	ldi disp1, 0b11011101
+	ret
+	
+DISPLAY2_3:
+	ldi disp1, 0b11111001
+	ret
+
+DISPLAY2_4:
+	ldi disp1, 0b01101011
+	ret
+
+DISPLAY2_5:
+	ldi disp1, 0b10111011
+	ret
+
+DISPLAY2_6:
+	ldi disp1, 0b10111111
+	ret
+
+DISPLAY2_7:
+	ldi disp1, 0b11101000
+	ret
+
+DISPLAY2_8:
+	ldi disp1, 0b11111111
+	ret
+
+DISPLAY2_9:
+	ldi disp1, 0b11101011
+	ret
